ff163863743d2eb9cf0bb003e14f7218dd9aad49
EFStats <157517664+EFStats@users.noreply.github.com>
2024-02-01 00:12:38 +0100

    Update plots.py

--- src/plots.py
+++ src/plots.py
@@ -137,7 +137,7 @@ def doubleplot(df: pd.core.frame.DataFrame) -> None:
     ax.set_ylabel(ylabel = "Count",
                   fontsize = s,
                   labelpad = 10)    
-    ax.set_ylim((0, 1000))
+    ax.set_ylim((0, 4000))
     ax.hlines(y      = [1000 * i for i in range(50)],
               xmin   = datetime.date(2024, 1, 1),
               xmax   = datetime.date(2024, 9, 18),
@@ -189,7 +189,7 @@ def doubleplot(df: pd.core.frame.DataFrame) -> None:
                    which     = "both",
                    labelsize = s,
                    pad       = 10)
-    ax.set_xlim((0,1000))
+    ax.set_xlim((0,4000))
  
     # y axis
     ax.set_ylabel(ylabel  = "")
